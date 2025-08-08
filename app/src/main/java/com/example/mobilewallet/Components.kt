0c410b89460da77054939b94b27d379e19d7e40d
Davido <davidepinzan3@gmail.com>
2025-08-08 20:30:14 +0200

    Added redirection from wallet to issuer

--- app/src/main/java/com/example/mobilewallet/Components.kt
+++ app/src/main/java/com/example/mobilewallet/Components.kt
@@ -1,9 +1,9 @@
 package com.example.mobilewallet
 
 import android.app.Application
-import android.content.Context
-import androidx.compose.foundation.clickable
 
+import androidx.compose.foundation.clickable
+import android.content.Context
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,9 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
-
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -21,25 +21,37 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FloatingActionButton
+
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
-
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
-
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.TextUnit
+
+
+
+
+
+
+
+
+
+
+
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
+
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -59,6 +71,7 @@ fun CredentialDialog(content: String, onDismissRequest: () -> Unit) {
     title = { Text(text = "Verifiable presentation:") },
     dismissButton = {
       TextButton(onClick = onDismissRequest) { Text("Dismiss") }},
+
     confirmButton = {
       val clipboardManager = LocalClipboardManager.current
       TextButton(onClick = {
@@ -68,6 +81,7 @@ fun CredentialDialog(content: String, onDismissRequest: () -> Unit) {
       }) { Text("Confirm") }
     },
     text = {
+
       SelectionContainer {
         Text(content, modifier = Modifier.verticalScroll(rememberScrollState()))
       }
@@ -77,6 +91,7 @@ fun CredentialDialog(content: String, onDismissRequest: () -> Unit) {
 }
 
 @Composable
+
 fun Greeting(name: String, modifier: Modifier = Modifier) {
   Text(
     text = "Hello $name!", modifier = modifier, fontSize = TextUnit(38.0f, TextUnitType.Sp)
@@ -86,6 +101,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun Subtitle() {
+
   Text(text = "Your wallets", fontSize = TextUnit(30.0f, TextUnitType.Sp))
 }
 
@@ -93,35 +109,49 @@ fun Subtitle() {
 fun AddButton(onClick: () -> Unit) {
   FloatingActionButton(onClick = onClick) {
     Icon(
-
       Icons.Filled.Add, "Add new wallet or digital credential"
     )
-  }
 
+  }
 }
 
 @Composable
 fun CardWallet(name: String, onClick: (String) -> Unit, delete: () -> Unit) {
   Card(
-
     modifier = Modifier
-      .fillMaxWidth()
 
+
+
+
+
+
+      .fillMaxWidth()
       .padding(16.00.dp)
       .height(100.0.dp)
       .clickable {onClick(name)}
   ) {
     Row() {
-      Text(name, Modifier.padding(16.00.dp))
 
+      Text(name, Modifier.padding(16.00.dp))
       IconButton(onClick = delete) {
         Icon(
           Icons.Filled.Delete, "Delete selected wallet"
-
         )
+
+
+
+
+
+
+
+
+
+
+
       }
     }
   }
+
 }
 
 @Composable
@@ -131,25 +161,13 @@ fun ListColumn(walletModel: WalletModel = viewModel(), onClick: (String) -> Unit
   if (wallets.isEmpty()) {
     Text("You don't have any wallets yet.")
   }
+
   else {
     LazyColumn {
-
       items(wallets) { name ->
         CardWallet(name, onClick) {
           walletModel.removeWallet(name)
         }
-
-
-
-
-
-
-
-
-
-
-
-
       }
     }
   }
@@ -183,8 +201,8 @@ fun AddWalletDialog(walletModel: WalletModel = viewModel(), onDismissRequest: ()
 //TODO: Remove
 class ButtonModel(application: Application) : AndroidViewModel(application) {
   private val _preferences = application.getSharedPreferences("did", Context.MODE_PRIVATE)
-  fun regenerate() {
 
+  fun regenerate() {
     viewModelScope.launch {
       val keydid = async {
         generateKeyDid()
@@ -193,8 +211,8 @@ class ButtonModel(application: Application) : AndroidViewModel(application) {
         val resolved = keydid.await()
         putString("did", resolved.second)
         putString("key", resolved.first.exportJWK())
-        apply()
 
+        apply()
       }
     }
   }
@@ -203,8 +221,8 @@ class ButtonModel(application: Application) : AndroidViewModel(application) {
 @Composable
 fun Regeneration(buttonModel: ButtonModel = viewModel()) {
   ElevatedButton(onClick = {
-    buttonModel.regenerate()
 
+    buttonModel.regenerate()
   }) {Text("Regenerate")}
 }
 
@@ -213,14 +231,27 @@ fun AddDocumentDialog(documentModel: DocumentModel, onDismissRequest: () -> Unit
   var value by remember { mutableStateOf("") }
   AlertDialog(
     onDismissRequest = onDismissRequest,
+
     title = { Text(text = "Import credential") },
     dismissButton = {
       TextButton(onClick = onDismissRequest) { Text("Dismiss") }
     },
 
+
+
+
+
+
+
+
+
+
+
+
     confirmButton = {
       TextButton(onClick = {
         documentModel.addDocument(value)
+
         onDismissRequest()
       }) { Text("Confirm") }
     },
@@ -230,24 +261,13 @@ fun AddDocumentDialog(documentModel: DocumentModel, onDismissRequest: () -> Unit
       )
     },
   )
+
 }
 
 @Composable
 fun CredentialCard(jwt: String, onClick: (String) -> Unit, delete: () -> Unit) {
   Card(
     modifier = Modifier
-
-
-
-
-
-
-
-
-
-
-
-
       .fillMaxWidth()
       .padding(16.00.dp)
       .height(100.0.dp)
@@ -266,6 +286,5 @@ fun CredentialCard(jwt: String, onClick: (String) -> Unit, delete: () -> Unit) {
         )
       }
     }
-
   }
 }
