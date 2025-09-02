797e2dd0e24b56828570a058626232c4ebd084c8
Davido <davidepinzan3@gmail.com>
2025-09-02 11:30:36 +0200

    Added access token verification in service

--- issuer/src/main/java/com/example/mobileissuer/Service.kt
+++ issuer/src/main/java/com/example/mobileissuer/Service.kt
@@ -10,6 +10,12 @@ import android.os.Message
 import android.os.Messenger
 import android.util.Log
 import androidx.core.content.edit
+import id.walt.crypto.keys.jwk.JWKKey
+
+import id.walt.oid4vc.OpenID4VC
+import id.walt.oid4vc.providers.TokenTarget
+import kotlinx.coroutines.runBlocking
+
 import kotlinx.serialization.json.Json
 
 class IssuanceService : Service() {
@@ -19,24 +25,18 @@ class IssuanceService : Service() {
   internal class HandlerIncoming(
     context: Context,
     val service: IssuanceService,
-    val applicationContext : Context = context.applicationContext,
+    val applicationContext: Context = context.applicationContext,
   ) : Handler() {
 
     override fun handleMessage(msg: Message) {
       val token = msg.data.getString("authorization")
-      val prefs = applicationContext.getSharedPreferences("session", Context.MODE_PRIVATE)
-      val session = tokenToPayload(token!!)["sub"].toString().removeSurrounding("\"")
-      val res = Message.obtain()
+      runBlocking {
 
-      res.data.putString(
-        "credentials",
-        Json.encodeToString(arrayOf(
-
-          (mutableMapOf("credential" to (prefs.getString(session, "") ?: "")))
-        ))
-      )
-      msg.replyTo.send(res)
-      prefs.edit(commit = true) {
+        val keySharedPrefs = applicationContext.getSharedPreferences("did", Context.MODE_PRIVATE)
+        val key = keySharedPrefs.getString("key", "")!!
+        val public = JWKKey.importJWK(key).getOrNull()!!.getPublicKey()
+        if (OpenID4VC.verifyTokenSignature(TokenTarget.ACCESS, token!!, public)) {
+          val sessionSharedPrefs =
 
 
 
@@ -48,11 +48,27 @@ class IssuanceService : Service() {
 
 
 
-        remove(session)
-        clear() //TODO remove
+            applicationContext.getSharedPreferences("session", Context.MODE_PRIVATE)
+          val session = tokenToPayload(token!!)["sub"].toString().removeSurrounding("\"")
+          val res = Message.obtain()
+
+          res.data.putString(
+            "credentials",
+            Json.encodeToString(
+              arrayOf(
+                (mutableMapOf("credential" to (sessionSharedPrefs.getString(session, "") ?: "")))
+              )
+            )
+          )
+          msg.replyTo.send(res)
+
+          sessionSharedPrefs.edit(commit = true) {
+            remove(session)
+            clear() //TODO remove
+          }
+          super.handleMessage(msg)
+        }
       }
-
-      super.handleMessage(msg)
     }
   }
 
